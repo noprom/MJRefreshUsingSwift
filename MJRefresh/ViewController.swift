591ad44d78f99b16220158079c7e7409e48ee976
noprom <tyee.noprom@qq.com>
2015-08-15 17:34:25 +0800

    add tableview

--- MJRefresh/ViewController.swift
+++ MJRefresh/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    var tableView:UITableView?
+    var objArray = [String]()
+    var i = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView = UITableView(frame: self.view.frame)
+        self.view.addSubview(tableView!)
+        self.tableView!.delegate = self
+        self.tableView!.dataSource = self
+        self.tableView!.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "cell")
+        self.tableView!.tableFooterView = UIView()
+        
+        // 初始化数组
+        for (i; i<10; ++i){
+            self.objArray.append("\(self.i)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel!.text = "这是第 \(self.objArray[indexPath.row]) 行"
+        return cell
+    }
 }
 
